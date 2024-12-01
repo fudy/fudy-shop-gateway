652729880dfb51f49da6c2533339078fd59aff13
Fudy <lifudong1@163.com>
2024-12-01 13:10:50 +0800

    bugfix for api management

--- fudy-shop-gateway-application/src/main/java/com/fudy/shop/gateway/application/dto/GatewayApiDTO.java
+++ fudy-shop-gateway-application/src/main/java/com/fudy/shop/gateway/application/dto/GatewayApiDTO.java
@@ -1,5 +1,17 @@
 package com.fudy.shop.gateway.application.dto;
 
-public class GatewayApiDTO {
+import lombok.Data;
 
+@Data
+public class GatewayApiDTO {
+    /** id */
+    private Long id;
+    /** 接口名 */
+    private String apiName;
+    /** 方法名 */
+    private String methodName;
+    /** 参数名 */
+    private String parameterTypes;
+    /** 版本号 */
+    private String version;
 }
